fcb89673249cf5357ac42153e082f726395aae8a
Ethan Wall <ewall25@jesuittampa.org>
2022-09-30 18:32:50 -0400

    Implemented new OI in RobotContainer

--- container.py
+++ container.py
@@ -1,7 +1,7 @@
-import commands2
 import commands2.button
 import wpilib
 
+from oi import XboxDriver, XboxOperator
 from subsystems import Drivetrain, Arm, Winch
 
 
@@ -19,28 +19,30 @@ class RobotContainer:
 
         # Joysticks are plugged into the driver laptop and used during the teleop period to control the robot
         # Each joystick is plugged into a port, ranging from 0 to 5
-        self.drive_stick = wpilib.XboxController(0)
-        self.arm_stick = wpilib.XboxController(1)
+
+        # The drive joystick controls the drivetrain (i.e. movement)
+        self.drive_stick = XboxDriver(0)
+
+        # The operator joystick controls the arm and winch
+        self.operator_stick = XboxOperator(1)
 
         # Default commands run whenever no other commands are scheduled
         # This included the teleop period, so code for teleop control should be set as the default command
         self.drivetrain.setDefaultCommand(
             # Drive the robot forwards and backwards with the left stick on an Xbox controller
             # Turn left and right with the right stick
+            # Hold either bumper to move slowly
             self.drivetrain.get_default_command(
-                # Y sticks report a value of -1 when the stick is pushed all the way up, so invert the value
-                lambda: -self.drive_stick.getLeftY(),
-                self.drive_stick.getRightX,
-                # If either the left or right bumper is pressed, drive and turn at a slower speed
-                lambda: self.drive_stick.getLeftBumper()
-                or self.drive_stick.getRightBumper(),
+                self.drive_stick.forward,
+                self.drive_stick.turn,
+                self.drive_stick.mod,
             )
         )
 
         self.arm.setDefaultCommand(
             # Control the arm motors' turning power with the left stick on an Xbox controller.
-            # Releasing the joystick causes the arm to slowly fall
-            self.arm.get_default_command(lambda: -self.arm_stick.getLeftY())
+            # Releasing the joystick causes the arm to fall slowly
+            self.arm.get_default_command(self.operator_stick.arm)
         )
 
         # Bind buttons to Commands
@@ -59,14 +61,10 @@ class RobotContainer:
         """Bind buttons on the Xbox controllers to run Commands"""
 
         # Wind the winch while holding "up" on the d-pad
-        commands2.button.POVButton(self.arm_stick, angle=0, povNumber=0).whenHeld(
-            self.winch.get_wind_command()
-        )
+        self.operator_stick.wind_winch.whenHeld(self.winch.get_wind_command())
 
         # Unwind the winch while holding "down" on the d-pad
-        commands2.button.POVButton(self.arm_stick, angle=180, povNumber=0).whenHeld(
-            self.winch.get_unwind_command()
-        )
+        self.operator_stick.unwind_winch.whenHeld(self.winch.get_unwind_command())
 
     def get_autonomous_command(self) -> commands2.Command:
         return self.chooser.getSelected()
